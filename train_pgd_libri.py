03f39dcc14707474db539486f3960d657a0600e6
monisankha <monisankha.pal@gmail.com>
2020-10-22 01:19:30 -0700

    Update train_pgd_libri.py

--- train_pgd_libri.py
+++ train_pgd_libri.py
@@ -206,7 +206,7 @@ def main(args):
     'targeted': False,
     'epsilon': args.epsilon,
     'num_steps': 1,
-    'step_size': args.epsilon / 5,
+    'step_size': args.epsilon,
     'random_start': True
     }  
 
